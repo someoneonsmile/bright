4f653eb45c798fac919271b4eafa0a0bcdea7c47
someoneonsmile <someoneonsmile@gmail.com>
2022-06-03 02:06:25 +0800

    fix: config file, use for_each_concurrent

--- src/config.rs
+++ src/config.rs
@@ -13,13 +13,20 @@ pub(crate) struct Config {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct DeviceConfig {
-    pub time_bright: Vec<(NaiveTime, u32)>,
+    pub time_bright: Vec<DeviceConfigItem>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct DeviceConfigItem {
+    pub time: NaiveTime,
+    pub bright: u32,
 }
 
 impl Config {
     /// parse config file
     pub fn from_toml<P: AsRef<Path>>(config_path: P) -> anyhow::Result<Option<Config>> {
         let config_path = util::shell_expend_full(config_path)?;
+        println!("config path: {:?}", config_path);
         if !config_path.exists() {
             return Ok(None);
         }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use chrono::Local;
 use config::DeviceConfig;
 use futures::{StreamExt, TryStreamExt};
 use std::collections::HashMap;
-use std::time::Duration;
 use tokio::time::{self, MissedTickBehavior};
 
 mod config;
@@ -13,7 +12,7 @@ mod util;
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
     let dev_map = get_dev_map().await?;
-    let config_path = "${XDG_CONFIG:~/.config}/bright/config.toml";
+    let config_path = "${XDG_CONFIG:-~/.config}/bright/config.toml";
     let config = config::Config::from_toml(config_path)?.ok_or_else(|| {
         anyhow::anyhow!(format!("can't not find the config file {}", config_path))
     })?;
@@ -22,16 +21,17 @@ async fn main() -> anyhow::Result<()> {
     show_brightnes(&dev_map).await?;
 
     // concurrent adjust the brightness
-    futures::stream::iter(dev_map)
-        .then(|(dev_name, dev)| async {
+    futures::stream::iter(dev_map.into_iter())
+        .map(Ok)
+        .try_for_each_concurrent(None, |(dev_name, dev)| async {
             let dev_config = config.dev.get(&dev_name).ok_or_else(|| {
                 anyhow::anyhow!(format!("can't find the config for device: {}", dev_name))
             })?;
             set_brightnes(dev_name, dev, dev_config).await?;
             Ok(()) as anyhow::Result<()>
         })
-        .try_collect()
         .await?;
+
     Ok(())
 }
 
@@ -51,7 +51,7 @@ async fn set_brightnes(
     mut dev: BrightnessDevice,
     dev_config: &config::DeviceConfig,
 ) -> anyhow::Result<()> {
-    let mut interval = time::interval(Duration::from_millis(200));
+    let mut interval = time::interval(std::time::Duration::from_millis(200));
     interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
     loop {
         interval.tick().await;
@@ -62,21 +62,19 @@ async fn set_brightnes(
             match cur_value {
                 cur_value if cur_value == target => {
                     let next_wake_time = get_next_wake_time(dev_config)?;
-                    let sleep_duration = next_wake_time
-                        .signed_duration_since(Local::now().time())
-                        .to_std()?;
+                    let mut sleep_duration =
+                        next_wake_time.signed_duration_since(Local::now().time());
+                    if sleep_duration < chrono::Duration::zero() {
+                        sleep_duration = sleep_duration + chrono::Duration::days(1);
+                    }
                     println!("sleep: {:?}, wake at {}", sleep_duration, next_wake_time);
-                    time::sleep(sleep_duration).await;
+                    time::sleep(sleep_duration.to_std()?).await;
                 }
                 _ => {
-                    // TODO: * 10 read from config
-                    let brightness = match (target as i32 - cur_value as i32) * 10 / 100 {
-                        0 => (cur_value as i32 + if target > cur_value { 1 } else { -1 }) as u32,
-                        v => cur_value + v as u32,
-                    };
+                    let brightness = calc_next_bright(cur_value, target);
                     println!(
-                        "Brightness of device {}, target={}%, set={}%",
-                        dev_name, target, brightness
+                        "Brightness of device {}, current={}%, target={}%, set={}%",
+                        dev_name, cur_value, target, brightness
                     );
                     dev.set(brightness).await?;
                 }
@@ -98,9 +96,9 @@ fn get_target_brightness(dev_config: &DeviceConfig) -> Option<u32> {
     dev_config
         .time_bright
         .iter()
-        .rfind(|it| it.0 < now)
+        .rfind(|it| it.time < now)
         .or_else(|| dev_config.time_bright.last())
-        .map(|it| it.1)
+        .map(|it| it.bright)
 }
 
 fn get_next_wake_time(dev_config: &DeviceConfig) -> anyhow::Result<NaiveTime> {
@@ -108,8 +106,16 @@ fn get_next_wake_time(dev_config: &DeviceConfig) -> anyhow::Result<NaiveTime> {
     dev_config
         .time_bright
         .iter()
-        .find(|it| it.0 > now)
+        .find(|it| it.time > now)
         .or_else(|| dev_config.time_bright.first())
-        .map(|it| it.0)
+        .map(|it| it.time)
         .ok_or_else(|| anyhow::anyhow!("not find the next wake time"))
+}
+
+fn calc_next_bright(cur_value: u32, target: u32) -> u32 {
+    // TODO: * 10 read from config
+    match (target as i32 - cur_value as i32) * 10 / 100 {
+        0 => (cur_value as i32 + if target > cur_value { 1 } else { -1 }) as u32,
+        v => cur_value + v as u32,
+    }
 }
