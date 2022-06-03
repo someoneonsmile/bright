b4fab11a40f4c13a0895537993a71116b8eded8c
someoneonsmile <someoneonsmile@gmail.com>
2022-06-04 04:22:36 +0800

    keep current when outside and fix calc easing overflow

--- src/util.rs
+++ src/util.rs
@@ -20,27 +20,33 @@ pub(crate) use shellexpand::tilde;
 
 pub(crate) fn easing(current_val: i32, set_val: i32, easing_percent: u32, min_step: u32) -> i32 {
     let min_step = min_step as i32;
-    let easing_set_val = match (set_val - current_val) * easing_percent as i32 / 100 {
+    let easing_percent = easing_percent as i32;
+    let easing_set_val = match (set_val - current_val) * easing_percent / 100 {
         0 => {
-            if set_val > current_val {
-                (set_val).min(current_val + min_step)
-            } else {
-                (set_val).max(current_val - min_step)
-            }
+            current_val + (set_val - current_val).signum() * min_step
+            // if set_val > current_val {
+            //     current_val + min_step
+            // } else {
+            //     current_val - min_step
+            // }
         }
-        // 如果超过 set_val, 则退回到 current_val
-        v if v > 0 => match current_val + v.abs().max(min_step) {
-            v if v > set_val => current_val,
-            _ => v,
-        },
-        // 如果超过 set_val, 则退回到 current_val
-        v if v < 0 => match current_val - v.abs().max(min_step) {
-            v if v < set_val => current_val,
-            _ => v,
-        },
-        _ => {
-            unreachable!()
-        }
+        v => current_val + v.signum() * v.abs().max(min_step),
     };
-    easing_set_val
+    if (easing_set_val - current_val).signum() * (easing_set_val - set_val).signum() > 0 {
+        current_val
+    } else {
+        easing_set_val
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_easing() {
+        // assert_eq!(-80 * 10 / 100, 0);
+        let r = easing(21, 20, 10, 2);
+        assert_eq!(r, 21);
+    }
 }
